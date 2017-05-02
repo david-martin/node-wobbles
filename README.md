11f5fd6486ad1fe257236fd657a722fa440cc35e
David Martin <david.martin@redhat.com>
2017-05-02 12:14:01 +0100

    Add garbage collection & heap stats script

--- README.md
+++ README.md
@@ -42,6 +42,41 @@ FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memo
 Aborted (core dumped)
 ```
 
+## Garbage collection stats
+
+Keep allocating memory, but allow it to be garbage collected.
+If `INFLUXDB_HOST` and `INFLUXDB_PORT` are set, various heap space & garbage collection stats will be sent to influxDB.
+Sample dashboard in ./grafana_gc_dashboard.json.
+This script uses the `gc-stats` node module to gather stats whenever a garbage collection happens. More info on those stats can be found in the readme https://www.npmjs.com/package/gc-stats
+
+```
+INFLUXDB_HOST=myinfluxhost INFLUXDB_PORT=8086 node --max_old_space_size=2048 use-memory-but-allow-gc.js
+1.1% 91932768 / 8455716864
+GC happened TYPE 1 BEFORE 4089400 AFTER 3526800
+GC happened TYPE 1 BEFORE 8639312 AFTER 7484704
+GC happened TYPE 1 BEFORE 9082184 AFTER 8329072
+GC happened TYPE 1 BEFORE 9346744 AFTER 8875352
+GC happened TYPE 1 BEFORE 12233144 AFTER 10571688
+GC happened TYPE 1 BEFORE 12731912 AFTER 11642408
+GC happened TYPE 1 BEFORE 15462728 AFTER 13731400
+GC happened TYPE 1 BEFORE 25507896 AFTER 23299640
+GC happened TYPE 1 BEFORE 27138904 AFTER 25859096
+GC happened TYPE 1 BEFORE 51413536 AFTER 46761568
+GC happened TYPE 1 BEFORE 53972992 AFTER 51569056
+1.5% 123730528 / 8455716864
+GC happened TYPE 1 BEFORE 74609560 AFTER 70247968
+GC happened TYPE 1 BEFORE 80854896 AFTER 75975624
+GC happened TYPE 1 BEFORE 97727536 AFTER 93739696
+GC happened TYPE 1 BEFORE 109231368 AFTER 106385896
+GC happened TYPE 1 BEFORE 117206344 AFTER 113599336
+GC happened TYPE 1 BEFORE 115450280 AFTER 115450280
+GC happened TYPE 1 BEFORE 115745272 AFTER 102115288
+GC happened TYPE 1 BEFORE 114214192 AFTER 113659120
+GC happened TYPE 1 BEFORE 115644752 AFTER 114982864
+GC happened TYPE 1 BEFORE 116754896 AFTER 116164208
+2.2% 184388448 / 8455716864
+```
+
 ## Trigger an ECONNRESET (socket hang up)
 
 There is a backend server (on port 8002) and a proxy server (on port 8001). The proxy server will proxy onto the backend server. The proxy server has a timeout of 1 second on any request it proxies to the backend server. The backend server is setup to wait 2 seconds before responding. This always results in a 'socket hang up' ECONNRESET.
---
+++ use-memory-but-allow-gc.js
@@ -0,0 +1,118 @@
+var v8 = require('v8');
+var keepAllocating = true;
+var x;
+var timeout = 500;
+var gc = (require('gc-stats'))();
+var fhComponentMetrics = require('fh-component-metrics');
+var metricsConf = {enabled: true, host: process.env.INFLUXDB_HOST, port: process.env.INFLUXDB_PORT};
+var metrics = fhComponentMetrics(metricsConf);
+
+
+/*
+{
+    pause: 433034,
+    pauseMS: 0,
+    gctype: 1,
+    before: {
+        totalHeapSize: 18635008,
+        totalHeapExecutableSize: 4194304,
+        usedHeapSize: 12222496,
+        heapSizeLimit: 1535115264
+    }, after: {
+        totalHeapSize: 18635008,
+        totalHeapExecutableSize: 4194304,
+        usedHeapSize: 8116600,
+        heapSizeLimit: 1535115264
+    }, diff: {
+        totalHeapSize: 0,
+        totalHeapExecutableSize: 0,
+         usedHeapSize: -4105896,
+        heapSizeLimit: 0
+    }
+}
+*/
+gc.on('stats', function (stats) {
+  console.log('GC happened TYPE', stats.gctype, 'BEFORE', stats.before.usedHeapSize, 'AFTER', stats.after.usedHeapSize);
+	metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'gctype'}, stats.gctype);
+	// metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'before.totalHeapSize'}, stats.before.totalHeapSize);
+	// metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'before.totalHeapExecutableSize'}, stats.before.totalHeapExecutableSize);
+	// metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'before.usedHeapSize'}, stats.before.usedHeapSize);
+	// metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'before.heapSizeLimit'}, stats.before.heapSizeLimit);
+	// metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'after.totalHeapSize'}, stats.after.totalHeapSize);
+	// metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'after.totalHeapExecutableSize'}, stats.after.totalHeapExecutableSize);
+	// metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'after.usedHeapSize'}, stats.after.usedHeapSize);
+	// metrics.gauge('NODEJS_MEMORY_TEST_gc', {'type': 'after.heapSizeLimit'}, stats.after.heapSizeLimit);
+});
+
+function alloc() {
+	x = [];
+	if (keepAllocating) {
+		for(var i=0, l=1000000; i<l; i++) {
+			x.push({test:'value'});
+		}
+	}
+
+	/*{ total_heap_size: 10504544,
+  total_heap_size_executable: 5242880,
+  total_physical_size: 7011248,
+  total_available_size: 1490995272,
+  used_heap_size: 5763808,
+  heap_size_limit: 1535115264 }*/
+	const stats = v8.getHeapStatistics();
+	const percentUsed = stats.total_heap_size / stats.heap_size_limit;
+	console.log((percentUsed * 100).toPrecision(2) + '%', stats.total_heap_size, '/', stats.heap_size_limit);
+	metrics.gauge('NODEJS_MEMORY_TEST_getHeapStatistics', {'type': 'percentUsed'}, percentUsed);
+	metrics.gauge('NODEJS_MEMORY_TEST_getHeapStatistics', {'type': 'total_heap_size'}, stats.total_heap_size);
+	metrics.gauge('NODEJS_MEMORY_TEST_getHeapStatistics', {'type': 'total_heap_size_executable'}, stats.total_heap_size_executable);
+	metrics.gauge('NODEJS_MEMORY_TEST_getHeapStatistics', {'type': 'total_physical_size'}, stats.total_physical_size);
+	metrics.gauge('NODEJS_MEMORY_TEST_getHeapStatistics', {'type': 'total_available_size'}, stats.total_available_size);
+	metrics.gauge('NODEJS_MEMORY_TEST_getHeapStatistics', {'type': 'used_heap_size'}, stats.used_heap_size);
+	metrics.gauge('NODEJS_MEMORY_TEST_getHeapStatistics', {'type': 'heap_size_limit'}, stats.heap_size_limit);
+
+/*
+> require('v8').getHeapSpaceStatistics()
+[ { space_name: 'new_space',
+    space_size: 4127744,
+    space_used_size: 1037472,
+    space_available_size: 1026400,
+    physical_space_size: 2134792 },
+  { space_name: 'old_space',
+    space_size: 2948480,
+    space_used_size: 2880808,
+    space_available_size: 0,
+    physical_space_size: 2930728 },
+  { space_name: 'code_space',
+    space_size: 1300640,
+    space_used_size: 1033344,
+    space_available_size: 0,
+    physical_space_size: 1082496 },
+  { space_name: 'map_space',
+    space_size: 1095744,
+    space_used_size: 232320,
+    space_available_size: 0,
+    physical_space_size: 265600 },
+  { space_name: 'large_object_space',
+    space_size: 0,
+    space_used_size: 0,
+    space_available_size: 1491119872,
+    physical_space_size: 0 } ]
+*/
+
+  const heapSpaceStatistics = v8.getHeapSpaceStatistics();
+	for (var i = 0, l = heapSpaceStatistics.length; i < l; i++) {
+		var heapSpace = heapSpaceStatistics[i];
+		metrics.gauge('NODEJS_MEMORY_TEST_getHeapSpaceStatistics_' + heapSpace.space_name, {type: 'space_size'}, heapSpace.space_size);
+		metrics.gauge('NODEJS_MEMORY_TEST_getHeapSpaceStatistics_' + heapSpace.space_name, {type: 'space_used_size'}, heapSpace.space_used_size);
+		metrics.gauge('NODEJS_MEMORY_TEST_getHeapSpaceStatistics_' + heapSpace.space_name, {type: 'space_available_size'}, heapSpace.space_available_size);
+		metrics.gauge('NODEJS_MEMORY_TEST_getHeapSpaceStatistics_' + heapSpace.space_name, {type: 'physical_space_size'}, heapSpace.physical_space_size);
+	}
+	
+	// if more than 20% is used, stop allocating
+	// if (percentUsed > 0.2) {
+	// 	keepAllocating = false;
+	// 	timeout = 1000;
+	// }
+  setTimeout(alloc, timeout);
+}
+
+alloc()
